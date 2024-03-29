07b2b1eb664a74197fca5e6ca61b0112d175adbc
Tim van Osch <timvanosch@pollex.nl>
2024-03-29 13:47:03 +0100

    Update board.h for newest proto

--- switch-firmware/boards/zoutwachter-proto/include/board.h
+++ switch-firmware/boards/zoutwachter-proto/include/board.h
@@ -3,4 +3,12 @@
 
 #include "cpu.h"
 
+#define LED_OUT GPIO_PIN(PORT_B, 13)
+#define LED_IN GPIO_PIN(PORT_A, 0)
+#define IOEXP_RST GPIO_PIN(PORT_A, 2)
+#define USB_CONNECTED GPIO_PIN(PORT_A, 15)
+#define BUCK_EN GPIO_PIN(PORT_B, 14)
+#define V5_EN GPIO_PIN(PORT_B, 15)
+
+
 #endif /* end of include guard: BOARD_H */
